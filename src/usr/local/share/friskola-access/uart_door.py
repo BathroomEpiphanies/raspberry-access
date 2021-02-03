5b0841398a459a43fb2ab42797c1f685f2667f9b
Fredrik Atmer <fatr@tobii.com>
2021-02-03 14:41:57 +0100

    Adding functionality for (new) "quatro" systems along with (old) "ssr" systems. Re-arranged /usr/local.

--- src/usr/local/share/friskola-access/uart_door.py
+++ src/usr/local/share/friskola-access/uart_door.py
@@ -7,31 +7,38 @@ import time
 import RPi.GPIO as GPIO
 import sqlite3
 
-from door_functions import request_access,door_configs,raw_to_rfid
+from door_functions import system_configurations,request_access,setup_relay_pins,setup_wiegand_pins,raw_to_rfid
 
 
 def main():
     parser = argparse.ArgumentParser(description='Door access system.')
     parser.add_argument('--database',
                         required = True,
-                        help     = 'Database file.')
+                        help     = 'Database file [/perm/database.sqlite].')
     parser.add_argument('--door-name',
                         required = True,
-                        help     = 'Door identifier.')
+                        help     = 'Computer identifier [door-djurhuset,door-prototype,..].')
+    parser.add_argument('--system-type',
+                        required = True,
+                        help     = 'Circuit board type [ssr,quatro,..].')
     parser.add_argument('--door-number',
                         required = True,
                         type     = int,
-                        help     = 'Physical door controller number [1,2,3,4].')
+                        help     = 'Physical door number [1,2,3,4].')
     args = parser.parse_args()
     
     database = sqlite3.connect(args.database,check_same_thread=False).cursor()
+    door_number = args.door_number
+    door_name = args.door_name
+    system_type = args.system_type
     
-    rfid,pin = request_access(args.door_number,database)
+    setup_relay_pins(system_type,door_number)
+    UART = serial.Serial(system_configurations[system_type][door_number]['serial_port'],9600)
+    rfid,pin = request_access(system_type,door_number,database)
     
     print(f'Starting: {time.ctime()}',flush=True)
     while True:
         try:
-            UART = serial.Serial(door_configs[args.door_number]['serial_port'],9600)
             UART.close()
             UART.open()
             while True:
@@ -40,8 +47,8 @@ def main():
                     rfid = raw_to_rfid(UART.read(16).decode('utf-8'))
                 elif c and ord(c) == 0x12:
                     pin = pin[-3:] + UART.read(1).decode('utf-8')
-                rfid,pin = request_access(args.door_number,database,args.door_name,rfid,pin)
-                    
+                rfid,pin = request_access(system_type,door_number,database,door_name,rfid,pin)
+        
         except KeyboardInterrupt:
             sys.exit(0)
         except:
