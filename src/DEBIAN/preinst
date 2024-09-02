d027e7e9bde4cfd17ce3f927daefa970970ce9f7
Fredrik Atmer <fredrik@atmer.se>
2024-09-02 16:32:24 +0200

    Add doors 5-8 to systemd jobs. And where they are referenced.

--- src/DEBIAN/preinst
+++ src/DEBIAN/preinst
@@ -3,7 +3,11 @@
 for service in raspberry-access-door1.service \
                raspberry-access-door2.service \
                raspberry-access-door3.service \
-               raspberry-access-door4.service
+               raspberry-access-door4.service \
+               raspberry-access-door5.service \
+               raspberry-access-door6.service \
+               raspberry-access-door7.service \
+               raspberry-access-door8.service
 do
     if systemctl --no-pager is-active $service
     then
--- src/usr/share/raspberry-access/raspberry-access-monitor.sh
+++ src/usr/share/raspberry-access/raspberry-access-monitor.sh
@@ -6,5 +6,9 @@
 /usr/bin/tmux "split-window" -t "raspberry-access:dashboard" -v             -- "tail -f /var/log/raspberry-access-door2.log"
 /usr/bin/tmux "split-window" -t "raspberry-access:dashboard" -v             -- "tail -f /var/log/raspberry-access-door3.log"
 /usr/bin/tmux "split-window" -t "raspberry-access:dashboard" -v             -- "tail -f /var/log/raspberry-access-door4.log"
+/usr/bin/tmux "split-window" -t "raspberry-access:dashboard" -v             -- "tail -f /var/log/raspberry-access-door5.log"
+/usr/bin/tmux "split-window" -t "raspberry-access:dashboard" -v             -- "tail -f /var/log/raspberry-access-door6.log"
+/usr/bin/tmux "split-window" -t "raspberry-access:dashboard" -v             -- "tail -f /var/log/raspberry-access-door7.log"
+/usr/bin/tmux "split-window" -t "raspberry-access:dashboard" -v             -- "tail -f /var/log/raspberry-access-door8.log"
 /usr/bin/tmux "select-layout" "even-vertical"
 tmux attach
